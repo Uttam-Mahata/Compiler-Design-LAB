b9e08f21652d5eeb1943e1f43930651dd31dfaf2
Uttam Mahata <uttambav@gmail.com>
2025-11-11 14:08:44 +0530

    Refactor assembly code to correct instruction count and optimize arithmetic operations; add comprehensive 8086 conversion summary documentation.

--- Assignment-06-TCG/assembly_code.asm
+++ Assignment-06-TCG/assembly_code.asm
@@ -17,7 +17,7 @@ MAIN PROC
     ; Generated code starts here
 
 ; Three-Address Code to Assembly Translation
-; Total Instructions: 20
+; Total Instructions: 7
 
 
 ; = 10, , a
@@ -26,83 +26,31 @@ MAIN PROC
 ; = 20, , b
     MOV BX, 20        ; Load constant 20
 
-; * b, 2, T0
-    MOV CX, 2        ; Load constant 2
-    MOV DX, BX        ; Copy operand
-    IMUL DX, CX       ; DX = BX * CX
-
-; + a, T0, T1
-    MOV CX, AX        ; Copy operand
-    ADD CX, DX        ; CX = AX + DX
-
-; = T1, , c
-    MOV [T0], DX      ; Store DX to T0
-    MOV DX, CX        ; c = T1
-
-; ifFalse c, , L0
-    CMP DX, 0         ; Compare c with 0
-    JE L0             ; Jump to L0 if equal (false)
-
-; - c, 5, T2
-    MOV [T1], CX      ; Store CX to T1
-    MOV CX, 5        ; Load constant 5
-    MOV [a], AX      ; Store AX to a
-    MOV AX, DX        ; Copy operand
-    SUB AX, CX        ; AX = DX - CX
-
-; = T2, , result
-    MOV CX, AX        ; result = T2
-
-; goto , , L1
-    JMP L1            ; Unconditional jump to L1
-
-; label_end , , L0
-L0:
-
-; = 0, , result
-    MOV CX, 0        ; Load constant 0
-
-; label_end , , L1
-L1:
-
-; label_begin , , L2
-L2:
-
-; ifFalse a, , L3
-    MOV [T2], AX      ; Store AX to T2
-    MOV AX, [a]      ; Load a into AX
-    CMP AX, 0         ; Compare a with 0
-    JE L3             ; Jump to L3 if equal (false)
-
-; - a, 1, T3
-    MOV [b], BX      ; Store BX to b
-    MOV BX, 1        ; Load constant 1
-    MOV [result], CX      ; Store CX to result
-    MOV CX, AX        ; Copy operand
-    SUB CX, BX        ; CX = AX - BX
-
-; = T3, , a
-    MOV AX, CX        ; a = T3
-
-; + result, a, T4
-    MOV BX, [result]      ; Load result into BX
-    MOV [T3], CX      ; Store CX to T3
+; uminus b, , T0
     MOV CX, BX        ; Copy operand
-    ADD CX, AX        ; CX = BX + AX
+    NEG CX            ; T0 = -b
 
-; = T4, , result
-    MOV BX, CX        ; result = T4
+; = 5, , c
+    MOV DX, 5        ; Load constant 5
 
-; goto , , L2
-    JMP L2            ; Unconditional jump to L2
+; * a, T0, T1
+    MOV [b], BX      ; Store BX to b
+    MOV BX, AX        ; Copy operand
+    IMUL BX, CX       ; BX = AX * CX
 
-; label_end , , L3
-L3:
+; + T1, c, T2
+    MOV [T0], CX      ; Store CX to T0
+    MOV CX, BX        ; Copy operand
+    ADD CX, DX        ; CX = BX + DX
+
+; = T2, , d
+    MOV [T1], BX      ; Store BX to T1
+    MOV BX, CX        ; d = T2
 
 ; Store all modified values back to memory
     MOV [a], AX      ; Store AX to a
-    MOV [result], BX      ; Store BX to result
-    MOV [T4], CX      ; Store CX to T4
+    MOV [d], BX      ; Store BX to d
+    MOV [T2], CX      ; Store CX to T2
     MOV [c], DX      ; Store DX to c
 
     ; Program termination
